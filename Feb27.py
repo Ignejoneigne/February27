eaddd0e27b203b2bd104cdf3da8c2218f027613e
Igne <igneee@gmail.com>
2023-03-02 18:34:01 +0200

    Igne doing homework 2nd of March

--- Feb27.py
+++ Feb27.py
@@ -1,7 +1,43 @@
+#Task1
+
 a = "Igne"
 b = "Hallo SheGoesTech"
 print(a)
 print(b)
 print(a + b)
+
+#Task5.1
+
+for x in range(0, 101, 7):
+    print(x)
+
+#Task5.2
+word = input("Enter a word: ")
+averse = word[:]
+reverse = word[::-1]
+print(reverse)
+
+
+#Task5.2.1
+
+def my_function(y):
+    return y[::-1]
+mytxt = my_function("task is done")
+print(mytxt)
+
+#Task5.3
+
+import math
+print(math.factorial(7))
+
+#Task5.3.1
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Input a number to compute the factorial :"))
+print(factorial(n))
 
 
