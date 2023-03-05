286e621dc1a5d60758b48e0f5e6891ec485c091c
Igne <igneee@gmail.com>
2023-03-05 14:43:46 +0200

    Igne's homework Python_basics_4 and Python_basics_6

---
+++ Python_basics_4, 6_tasks.py
@@ -0,0 +1,59 @@
+#Practical task 6.1 (subtract 5 days from the current date)
+
+from datetime import datetime, timedelta
+current_date = datetime.now()
+print("Current date: ", str(current_date))
+date_5days_ago = current_date - timedelta(days=5)
+print("Date 5 days ago: ", str(date_5days_ago))
+
+
+#Practical task 4.1 (write function to find the Max of 3 numbers)
+
+def max_functions():
+    return max(9, 99, 999)
+print(max_functions())
+
+
+#Practical task 4.2 (write a program to reverse a string)
+
+txt = "1234abcd" [::-1]
+print(txt)
+
+#Practical task 4.3 (write a function to calculate the sum of all numbers passed as its arguments)
+
+def sum_numbers (*numbers:float) -> float:
+    return (sum(numbers))
+"""function calculates the sum of all numbers passed as its arguments"""
+print(sum_numbers(1, 2, 3))
+print(sum_numbers(8, 20, 2))
+print(sum_numbers(12.5, 3.147, 98.1))
+print(sum_numbers(1.1, 2.2, 5.5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
